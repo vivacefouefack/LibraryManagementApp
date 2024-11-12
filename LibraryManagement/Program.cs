f48f5a400fae2eed09c94d22b208deabe72e2a86
vivacefouefack <144936927+vivacefouefack@users.noreply.github.com>
2024-11-12 19:08:10 +0100

    addition of dependencies necessary for proper functioning

--- LibraryManagement/Program.cs
+++ LibraryManagement/Program.cs
@@ -3,7 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Learn more about configuring Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
