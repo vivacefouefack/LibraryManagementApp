6244773d3a25397d85a6fd9819198b3eae7eca5c
vivacefouefack <144936927+vivacefouefack@users.noreply.github.com>
2024-11-12 19:36:53 +0100

    creating the user class

---
+++ LibraryManagement/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement.Models
+{
+    public class User
+    {
+        private int Id { get; }
+
+        private string name { get; } = null!;
+
+        private string address { get; set; } = null!;
+
+        private string phoneNumber { get; set; } = null!;
+
+        private string email { get; } = null!;
+    }
+}
