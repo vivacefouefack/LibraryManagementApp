dd450106e31904b0d920704b0e4169eec5f5699a
vivacefouefack <144936927+vivacefouefack@users.noreply.github.com>
2024-11-12 22:33:25 +0100

    configuration and implementation of elements for database generation

---
+++ LibraryManagement/Database/LibraryDbContext.cs
@@ -0,0 +1,33 @@
+﻿using LibraryManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Database
+{
+    public class LibraryDbContext : DbContext
+    {
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+
+        protected LibraryDbContext(DbContextOptions<LibraryDbContext> options): base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Loan>()
+                .HasOne(l => l.Book)
+                .WithMany()  
+                .HasForeignKey(l => l.BookId);
+
+            modelBuilder.Entity<Loan>()
+                .HasOne(l => l.Member)
+                .WithMany()  
+                .HasForeignKey(l => l.MemberId);
+        }
+    }
+}
--- LibraryManagement/Program.cs
+++ LibraryManagement/Program.cs
@@ -1,8 +1,30 @@
+using LibraryManagement.Database;
+using LibraryManagement.Interfaces;
+using LibraryManagement.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddScoped<IMember, MemberService>();
+
+
+//Db config
+builder.Services.AddDbContext<LibraryDbContext>(option =>
+{
+    var ConnectionStrings = builder.Configuration.GetConnectionString("local");
+    option.UseSqlServer(ConnectionStrings);
+    //option.UseMySql(ConnectionStrings);
+});
+
+//cycle
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+});
+
 
 // Learn more about configuring Swagger
 builder.Services.AddEndpointsApiExplorer();
